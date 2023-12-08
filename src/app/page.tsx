91ea8439789a618de1b912cb192d7fae84ceffef
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-12-08 20:25:21 +0200

    feat(app): edit main page
    Added responsive styles, implemented SignInUp component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import SignInUp from '@/components/sign-in-up/SignInUp'
 import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.authorization}>
-      Login
+      <SignInUp />
     </main>
   )
 }
