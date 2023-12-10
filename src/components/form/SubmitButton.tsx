fe5c595e2e436819cfae89278ba720ab57358ba4
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-12-10 20:45:03 +0200

    feat(components): improve form's submit button
    Added pending state while executing action and corresponding visualization of that

--- src/components/form/SubmitButton.tsx
+++ src/components/form/SubmitButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './form.module.scss'
 import { motion, spring } from 'framer-motion'
+import { useFormStatus } from 'react-dom';
 
 interface SubmitButtonProps {
 	onSubmit: React.FormEventHandler,
@@ -8,11 +9,16 @@ interface SubmitButtonProps {
 }
 
 const SubmitButton = ({ onSubmit, text }: SubmitButtonProps) => {
+	const { pending } = useFormStatus();
+
 	return (
 		<motion.button
 			transition={{ type: spring }}
 			whileHover={{ scale: 1.025 }}
 			whileTap={{ scale: 0.95 }}
+
+			variants={{ disabled: { opacity: 0.75 } }}
+			animate={pending && 'disabled'}
 
 			className={styles.submitButton}
 			onSubmit={onSubmit}
