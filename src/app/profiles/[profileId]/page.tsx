d24840155e2e29e2b1c6804ce6f0a5f1808e6f32
JustFantick <68466135+JustFantick@users.noreply.github.com>
2024-01-08 11:04:27 +0200

    feat(app): edit ProfilePage
    Implemented store and set client profile data from server

--- src/app/profiles/[profileId]/page.tsx
+++ src/app/profiles/[profileId]/page.tsx
@@ -1,34 +1,66 @@
 import React from 'react'
 import prisma from '../../../../lib/prisma'
+import { useProfileDataStore } from '@/store/userProfileData';
 
 interface ProfilePageProps {
-	params: {
-		profileId: string,
-	},
+	params: { profileId: string },
 }
 
-const PrifilePage = async ({ params }: ProfilePageProps) => {
+const ProfilePage = async ({ params }: ProfilePageProps) => {
+	const parsedProfileId = parseInt(params.profileId);
+	const profileData = await prisma.users.findFirst({ where: { userId: parsedProfileId } });
+	const profileTasks = await prisma.tasks.findMany({ where: { userId: parsedProfileId } });
+	const profileLists = await prisma.lists.findMany({ where: { userId: parsedProfileId } });
 
-	const profileData = await prisma.users.findFirst({
-		where: {
-			userId: parseInt(params.profileId),
+	const tasksToStore = profileTasks.map((task) => {
+		return {
+			taskId: task.taskId,
+			name: task.taskName,
+			note: task.note,
+			isCompleted: task.isCompleted,
+			priority: task.priority,
+			editTime: task.editTime,
+			executeDate: task.executeDate,
+			listId: task.listId,
 		}
 	});
 
-	console.log(profileData);
+	if (profileData !== null) {
+		useProfileDataStore.setState({
+			userData: {
+				userId: profileData?.userId,
+				login: profileData?.login,
+				email: profileData?.email
+			},
+			userTasks: tasksToStore,
+			filteredTasks: tasksToStore,
+			userLists: profileLists.map(list => {
+				return {
+					listId: list.listId,
+					name: list.listName,
+				}
+			}),
+		});
 
-	return (
-		<div>
-			<h1>Profile page</h1>
+		return (
+			<div>
+				<h2>Profile page</h2>
+				<p>
+					{JSON.stringify(useProfileDataStore.getState().userTasks)}
+				</p>
 
-			<ul>
-				<li>user id: {profileData?.userId}</li>
-				<li>login: {profileData?.login}</li>
-				<li>email: {profileData?.email}</li>
-				<li>password: {profileData?.password}</li>
-			</ul>
+			</div>
+		)
+	} else return (
+		<div style={{
+			height: '100%',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+		}}>
+			<h2>User not found</h2>
 		</div>
 	)
 }
 
-export default PrifilePage
+export default ProfilePage
