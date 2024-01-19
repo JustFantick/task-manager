217805a19e250d3af2c96e10b14b14d6bd8368fc
JustFantick <68466135+JustFantick@users.noreply.github.com>
2024-01-19 12:50:58 +0200

    fix(components): edit TasksListCard
    Changed props list, replaced counter block by removeList button

--- src/components/tasks-list-card/TasksListCard.tsx
+++ src/components/tasks-list-card/TasksListCard.tsx
@@ -4,26 +4,33 @@ import { motion } from 'framer-motion'
 
 interface TasksListCardProps {
 	listName: string,
-	listItemsNumber: number,
-	onClickHandler?: MouseEventHandler,
-	isActive?: boolean
+	onClickHandler: MouseEventHandler,
+	isActive: boolean,
+	removeListHandler?: MouseEventHandler,
 }
 
 const TasksListCard = ({
-	listName, listItemsNumber, onClickHandler, isActive = false
+	listName, onClickHandler, isActive, removeListHandler
 }: TasksListCardProps) => {
 	return (
 		<motion.div className={styles.tasksListCard} onClick={onClickHandler}
-			animate={isActive && {
-				borderBottom: '2px solid #7CB9DF',
+			animate={isActive ? {
+				borderBottom: '2px solid rgb(124, 185, 223)',
 				backgroundColor: 'rgb(57, 58, 60)',
+			} : {
+				borderBottom: '2px solid rgba(124, 185, 223, 0)',
+				backgroundColor: 'rgba(57, 58, 60, 0)',
 			}}
 			whileHover={{
 				backgroundColor: 'rgb(57, 58, 60)',
 			}}
 		>
 			<div className={styles.tasksListCard__name}>{listName}</div>
-			<div className={styles.tasksListCard__counter}>{listItemsNumber}</div>
+
+			{
+				removeListHandler &&
+				<div className={styles.tasksListCard__deleteBtn} onClick={removeListHandler}></div>
+			}
 
 		</motion.div>
 	)
