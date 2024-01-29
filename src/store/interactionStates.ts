3cbb4f0fe92390ee0ee1de860595dc245db044a0
JustFantick <68466135+JustFantick@users.noreply.github.com>
2024-01-29 10:48:27 +0200

    feat(store): edit interactionStates store
    Add setter methods, set default value of chosenListName variable

--- src/store/interactionStates.ts
+++ src/store/interactionStates.ts
@@ -2,12 +2,22 @@ import { create } from 'zustand'
 
 export const useInteractionStates = create<{
 	isTaskPopupOpen: boolean,
-	chosenList: string,
+	chosenListName: string,
 	isMobile: boolean,
 	isSideMenuOpen: boolean,
+
+	setIsTaskPopupOpen: (status: boolean) => void,
+	setChosenListName: (listName: string) => void,
+	setIsMobile: (status: boolean) => void,
+	setIsSideMenuOpen: (status: boolean) => void,
 }>((set) => ({
 	isTaskPopupOpen: false,
-	chosenList: '',
+	chosenListName: 'All tasks',
 	isMobile: false,
 	isSideMenuOpen: false,
+
+	setIsTaskPopupOpen: (status) => set(({ isTaskPopupOpen: status })),
+	setChosenListName: (listName) => set(({ chosenListName: listName })),
+	setIsMobile: (status) => set(({ isMobile: status })),
+	setIsSideMenuOpen: (status) => set(({ isSideMenuOpen: status })),
 }));
