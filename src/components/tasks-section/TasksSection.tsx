b9d475b22ac58d76be8c3059298d2646d179fe54
JustFantick <68466135+JustFantick@users.noreply.github.com>
2024-01-29 11:56:28 +0200

    feat(components): implement interactionsStore into main TasksSection

--- src/components/tasks-section/TasksSection.tsx
+++ src/components/tasks-section/TasksSection.tsx
@@ -4,17 +4,20 @@ import MainTitle from '../main-title/MainTitle'
 import { useProfileDataStore } from '@/store/userProfileData'
 import AddTask from '../add-task-field/AddTask'
 import TaskCart from '../task/TaskCart'
+import { useInteractionStates } from '@/store/interactionStates'
 
 const TasksSection = () => {
 	const { filteredTasks } = useProfileDataStore();
+	const { chosenListName } = useInteractionStates();
 
 	return (
 		<main className={styles.taskSection}>
-			<MainTitle text='Monday, 17th' />
+			<MainTitle text={chosenListName} />
 
 			<ul className={styles.taskSection__tasksList}>
 				{filteredTasks.map(task => (
-					<TaskCart taskId={task.taskId}
+					<TaskCart key={task.taskId}
+						taskId={task.taskId}
 						title={task.name}
 						isComplete={task.isCompleted}
 						isPrioritize={task.priority}
