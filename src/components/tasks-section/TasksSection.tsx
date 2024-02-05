49794b559b7c3c19bb2aada0b60ed68325770fe8
JustFantick <kotelevskijdanil0@gmail.com>
2024-02-05 11:28:05 +0200

    feat(components): modify TasksSection component
    Implemented render of current tasksList, added handlers to TaskCart that changes task complete, priority or chooses task as active for TaskPopup

--- src/components/tasks-section/TasksSection.tsx
+++ src/components/tasks-section/TasksSection.tsx
@@ -1,15 +1,80 @@
-import React from 'react'
+import React, { startTransition, useOptimistic } from 'react'
 import styles from './tasksSection.module.scss'
 import MainTitle from '../main-title/MainTitle'
-import { useProfileDataStore } from '@/store/userProfileData'
+import { Task, useProfileDataStore } from '@/store/userProfileData'
 import AddTask from '../add-task-field/AddTask'
 import TaskCart from '../task/TaskCart'
 import { useInteractionStates } from '@/store/interactionStates'
 import MenuBurger from '../menu-burger/MenuBurger'
+import { changeTaskComplete, changeTaskPriority } from '@/server-actions/task-actions'
 
 const TasksSection = () => {
-	const { filteredTasks } = useProfileDataStore();
-	const { chosenListName, setIsSideMenuOpen, isMobile } = useInteractionStates();
+	const { userTasks, setUserTasks, setTaskPopupId } = useProfileDataStore();
+	const { chosenListName, setIsSideMenuOpen, isMobile, setIsTaskPopupOpen } = useInteractionStates();
+
+	const [optimisticTasks, setOptimisticTasks] = useOptimistic(
+		userTasks,
+		(state, newList: Task[]) => newList,
+	)
+
+	function taskClickHandler(taskId: number) {
+		setTaskPopupId(taskId);
+		setIsTaskPopupOpen(true);
+	}
+
+	async function completeClickHandler(taskId: number) {
+		let newBooleanValue: boolean = false;
+		startTransition(() => {
+			setOptimisticTasks(userTasks.map(task => {
+				if (task.taskId === taskId) {
+					newBooleanValue = !task.isCompleted;
+					return {
+						...task,
+						isCompleted: newBooleanValue,
+					}
+				} else return task;
+			}));
+		});
+
+		const response = await changeTaskComplete(taskId, newBooleanValue);
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskId) {
+					return {
+						...task,
+						isCompleted: newBooleanValue,
+					}
+				} else return task;
+			}));
+		}
+	}
+
+	async function priorityClickHandler(taskId: number) {
+		let newBooleanValue = false;
+		startTransition(() => {
+			setOptimisticTasks(userTasks.map(task => {
+				if (task.taskId === taskId) {
+					newBooleanValue = !task.priority;
+					return {
+						...task,
+						priority: newBooleanValue,
+					}
+				} else return task;
+			}));
+		});
+
+		const response = await changeTaskPriority(taskId, newBooleanValue);
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskId) {
+					return {
+						...task,
+						priority: newBooleanValue,
+					}
+				} else return task;
+			}));
+		}
+	}
 
 	return (
 		<main className={styles.taskSection}>
@@ -22,12 +87,15 @@ const TasksSection = () => {
 			</div>
 
 			<ul className={styles.taskSection__tasksList}>
-				{filteredTasks.map(task => (
+				{optimisticTasks.map(task => (
 					<TaskCart key={task.taskId}
-						taskId={task.taskId}
 						title={task.name}
 						isComplete={task.isCompleted}
 						isPrioritize={task.priority}
+
+						taskClickHandler={() => taskClickHandler(task.taskId)}
+						completeChangeHandler={() => completeClickHandler(task.taskId)}
+						priorityChangeHandler={() => priorityClickHandler(task.taskId)}
 					/>
 				))}
 
