e04d48f5692a1e0dcfa370ec10dcb79b910d1eed
JustFantick <kotelevskijdanil0@gmail.com>
2024-02-11 17:39:20 +0200

    feat: create auth-session.ts file
    It provides functions to manage local authorization data

---
+++ lib/auth-session.ts
@@ -0,0 +1,62 @@
+"use server";
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+const secretKey = "secret";
+const key = new TextEncoder().encode(secretKey);
+
+const getExpiringTerm = (date: Date) => {
+	return date.setDate(date.getDate() + 7);
+};
+
+export async function encrypt(payload: any) {
+	return await new SignJWT(payload)
+		.setProtectedHeader({ alg: "HS256" })
+		.setIssuedAt()
+		.setExpirationTime("1 week from now")
+		.sign(key);
+}
+
+export async function decrypt(input: string): Promise<any> {
+	const { payload } = await jwtVerify(input, key, {
+		algorithms: ["HS256"],
+	});
+	return payload;
+}
+
+export async function login(formData: FormData) {
+	const user = { login: formData.get("login") };
+
+	const expires = getExpiringTerm(new Date);
+	const session = await encrypt({ user, expires });
+
+	cookies().set("session", session, { expires, httpOnly: true });
+}
+
+export async function logout() {
+	cookies().set("session", "", { expires: new Date(0) });
+}
+
+export async function getSession() {
+	const session = cookies().get("session")?.value;
+	if (!session) return null;
+	return await decrypt(session);
+}
+
+export async function updateSession(request: NextRequest) {
+	const session = request.cookies.get("session")?.value;
+	if (!session) return;
+
+	// Refresh the session so it doesn't expire
+	const parsed = await decrypt(session);
+	parsed.expires = getExpiringTerm(new Date);
+	const res = NextResponse.next();
+	res.cookies.set({
+		name: "session",
+		value: await encrypt(parsed),
+		httpOnly: true,
+		expires: parsed.expires,
+	});
+	return res;
+}
