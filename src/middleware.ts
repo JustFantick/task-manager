0e1b42de53a45cf92de88501cfd5029d4fa801de
JustFantick <kotelevskijdanil0@gmail.com>
2024-02-15 20:54:32 +0200

    feat: update middleware.ts
    Check 2 main routes and depending on auth cookies redirect user to needed page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,22 @@
-import { NextRequest } from "next/server";
-import { updateSession } from "../lib/auth-session";
+import { NextRequest, NextResponse } from "next/server";
+import { updateSession, decrypt } from "../lib/auth-session";
 
 export async function middleware(request: NextRequest) {
-	return await updateSession(request);
+	const session = await request.cookies.get('session')?.value;
+	const response = await updateSession(request);
+
+	if (!response && request.nextUrl.pathname.startsWith('/profiles')) {
+		return NextResponse.redirect(new URL('/', request.url));
+	} else if (response && !request.nextUrl.pathname.startsWith('/profiles') && session) {
+		const parsed = await decrypt(session);
+		if (parsed.user.rememberUser === "on") {
+			return NextResponse.redirect(new URL(`/profiles/${parsed.user.userId}`, request.url));
+		}
+	} else {
+		return response;
+	}
 }
 
 export const config = {
-	matcher: '/profiles/:path*',
+	matcher: ['/', '/profiles/:path*'],
 }
