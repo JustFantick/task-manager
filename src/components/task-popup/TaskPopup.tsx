abfc11e6932f49091a1cc39ebb070f08cc425e1b
JustFantick <kotelevskijdanil0@gmail.com>
2024-02-16 21:06:05 +0200

    refactor(components): implement PopupContainer into existing popups
    Implemented component into Task and DeleteTask popups

--- src/components/task-popup/TaskPopup.tsx
+++ src/components/task-popup/TaskPopup.tsx
@@ -1,40 +1,206 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './taskPopup.module.scss'
-import { AnimatePresence, motion } from 'framer-motion'
-import TaskPopupContent from './TaskPopupContent'
+import { useProfileDataStore } from '@/store/userProfileData'
+import HorizontalLine from '../horizontal-line/HorizontalLine'
+import ArrowLeftIcon from '../../../public/arrow_left.svg'
+import BinIcon from '../../../public/bin.svg'
+import NoteSection from './NoteSection'
+import TaskSection from './TaskSection'
+import DatePickerSection from './DatePickerSection'
 import { useInteractionStates } from '@/store/interactionStates'
+import { changeTaskComplete, changeTaskExecutionDate, changeTaskName, changeTaskNote, deleteTask } from '@/server-actions/task-actions'
+import { changeStepComplete, changeStepName, createStep, deleteStep } from '@/server-actions/step-actions'
+import DeleteTaskPopup from './delete-task-popup/DeleteTaskPopup'
+import PopupContainer from '../popup-container/PopupContainer'
 
-const TaskPopup = () => {
-	const { isTaskPopupOpen, setIsTaskPopupOpen } = useInteractionStates();
+const TaskPopupContent = () => {
+	const [isDeletePopupOpen, setIsDeletePopupOpen] = useState<boolean>(false);
+	const { taskPopupId, userTasks, setUserTasks, setTaskPopupId } = useProfileDataStore();
+	const { setIsTaskPopupOpen, isTaskPopupOpen } = useInteractionStates();
+
+	const task = userTasks.find(task => task.taskId === taskPopupId);
+
+	async function taskNameChangeHandler(newName: string) {
+		const response = await changeTaskName(taskPopupId, newName);
+
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskPopupId) {
+					return { ...task, name: newName };
+				} else return task;
+			}));
+		}
+	}
+
+	async function taskCompleteChangeHandler(completeStatus: boolean) {
+		const response = await changeTaskComplete(taskPopupId, completeStatus);
+
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskPopupId) {
+					return { ...task, isCompleted: completeStatus };
+				} else return task;
+			}));
+		}
+	}
+
+	async function stepNameChangeHandler(stepId: number, newName: string) {
+		const response = await changeStepName(stepId, newName);
+
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskPopupId) {
+					return {
+						...task,
+						steps: task.steps.map(step => {
+							if (step.stepId === stepId) {
+								return { ...step, stepName: newName };
+							} else return step;
+						})
+					};
+				} else return task;
+			}));
+		}
+	}
+
+	async function stepCompleteChangeHandler(stepId: number, completeStatus: boolean) {
+		const response = await changeStepComplete(stepId, completeStatus);
+
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskPopupId) {
+					return {
+						...task,
+						steps: task.steps.map(step => {
+							if (step.stepId === stepId) {
+								return { ...step, isCompleted: completeStatus };
+							} else return step;
+						})
+					};
+				} else return task;
+			}));
+		}
+	}
+
+	async function createStepHandler(stepName: string) {
+		const response = await createStep(taskPopupId, stepName);
+
+		if (response.success && response.createdStep) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskPopupId) {
+					return { ...task, steps: [...task.steps, response.createdStep] };
+				} else return task;
+			}))
+		}
+	}
+
+	async function changeNoteHandler(noteText: string) {
+		const response = await changeTaskNote(taskPopupId, noteText);
+
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskPopupId) {
+					return { ...task, note: noteText };
+				} else return task;
+			}))
+		}
+	}
+
+	async function changeDateHandler(date: Date | null) {
+		const response = await changeTaskExecutionDate(taskPopupId, date);
+
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskPopupId) {
+					return { ...task, executeDate: date };
+				} else return task;
+			}));
+		}
+	}
+
+	async function deleteStepHandler(stepId: number) {
+		const response = await deleteStep(stepId);
+
+		if (response.success) {
+			setUserTasks(userTasks.map(task => {
+				if (task.taskId === taskPopupId) {
+					return { ...task, steps: task.steps.filter(step => step.stepId !== stepId) };
+				} else return task;
+			}));
+		}
+	}
+
+	async function deleteTaskHandler() {
+		const response = await deleteTask(taskPopupId);
+
+		if (response.success) {
+			setUserTasks(userTasks.filter(task => task.taskId !== taskPopupId));
+			setTaskPopupId(0);
+			setIsTaskPopupOpen(false);
+		}
+	}
+
+	if (taskPopupId === 0 || taskPopupId === null || !task) return null;
 
 	return (
-		<AnimatePresence>
-			{
-				isTaskPopupOpen &&
-				<motion.div className={styles.taskPopup}
-					onClick={(e) => {
-						if (e.target === e.currentTarget) setIsTaskPopupOpen(false);
-					}}
-					key="task-popup"
-					initial={{ backgroundColor: 'rgba(0,0,0, 0)' }}
-					animate={{ backgroundColor: 'rgba(0,0,0, 0.5)' }}
-					exit={{ backgroundColor: 'rgba(0,0,0, 0)' }}
-				>
-					<motion.div
-						id='task-popup-content'
-						initial={{ x: '100%' }}
-						animate={{ x: 0 }}
-						exit={{ x: '100%' }}
-						transition={{ type: 'tween' }}
-					>
-						<TaskPopupContent />
-					</motion.div>
+		<>
+			<PopupContainer
+				isOpen={isTaskPopupOpen}
+				closePopup={() => setIsTaskPopupOpen(false)}
+				contentAppearance='rightToLeft'
+			>
+				<div className={styles.taskPopupContent}>
+					<div className={styles.taskPopupContent__dataSections}>
+						<TaskSection
+							taskName={task.name}
+							onTaskNameChange={taskNameChangeHandler}
+							isTaskComplete={task.isCompleted}
+							onTaskCompleteChange={taskCompleteChangeHandler}
 
-				</motion.div>
+							stepsList={task.steps}
+							onStepNameChange={stepNameChangeHandler}
+							onStepCompleteChange={stepCompleteChangeHandler}
 
-			}
-		</AnimatePresence>
+							stepDeleteHandler={deleteStepHandler}
+							stepCreateHandler={createStepHandler}
+						/>
 
+						<NoteSection note={task.note ? task.note : ''} setNote={changeNoteHandler} />
+
+						<DatePickerSection dateValue={task.executeDate} setDateValue={changeDateHandler} />
+
+					</div>
+
+
+					<HorizontalLine />
+					<div className={styles.taskPopupContent__footer}>
+						<ArrowLeftIcon className={styles.arrowLeftIcon} onClick={() => setIsTaskPopupOpen(false)} />
+
+						<div>Last edit: <time dateTime=''>
+							{new Intl.DateTimeFormat('en-US', {
+								hour: '2-digit',
+								minute: '2-digit',
+								hour12: false,
+							}).format(task.editTime)}
+						</time>
+						</div>
+
+						<BinIcon className={styles.binIcon} onClick={() => setIsDeletePopupOpen(true)} />
+
+					</div>
+
+				</div>
+
+			</PopupContainer>
+
+			<DeleteTaskPopup
+				taskName={task.name}
+				isOpen={isDeletePopupOpen}
+				setIsOpen={setIsDeletePopupOpen}
+				deleteTask={deleteTaskHandler}
+			/>
+		</>
 	)
 }
-export default TaskPopup
+
+export default TaskPopupContent
