8489f2c8671fd92b9313a71b9fa8e3f026ae60f0
JustFantick <kotelevskijdanil0@gmail.com>
2024-02-17 11:57:20 +0200

    feat: create nodemailer config file

---
+++ lib/nodemailer.ts
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer';
+
+const email = process.env.EMAIL;
+const pass = process.env.PASSWORD;
+
+export const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: email,
+		pass: pass,
+	}
+});
